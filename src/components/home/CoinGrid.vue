4a5f4b4474f4745be798186aaa9bfc86a4901e8e
OmniscientARK <50929077+OmniscientARK@users.noreply.github.com>
2022-01-13 06:48:14 -0300

    -

--- src/components/home/CoinGrid.vue
+++ src/components/home/CoinGrid.vue
@@ -7,8 +7,8 @@
       </div>
       <div class="filters">
         <div>
-<!--          <span>Update Delay:</span>-->
-<!--          <OutlineButton class="btn" @click.native="updateDelay = 1"><w2>1s</w2></OutlineButton>-->
+          <!--          <span>Update Delay:</span>-->
+          <!--          <OutlineButton class="btn" @click.native="updateDelay = 1"><w2>1s</w2></OutlineButton>-->
           <OutlineButton class="btn" @click.native="updateDelay = 3"><w2>3s</w2></OutlineButton>
           <OutlineButton class="btn" @click.native="updateDelay = 5"><w2>5s</w2></OutlineButton>
           <OutlineButton class="btn" @click.native="updateDelay = 10"><w2>10s</w2></OutlineButton>
@@ -17,15 +17,13 @@
         <OutlineButton class="btn"><w3>Advanced Mode</w3></OutlineButton>
       </div>
     </div>
-    <div class="grid" v-if="!received">
+    <div ref="grid" class="grid" v-if="!received">
       <CoinCard  v-for="i in 9" :key="i" :placeholder="true"/>
     </div>
-    <div class="grid" v-if="received">
-      <CoinCard v-for="token in tokens" :key="token.address" :data="token"/>
+    <div ref="grid" class="grid" v-if="received">
+      <CoinCard v-for="token in tokens.slice(0, getCurrentlyLoaded(tokens))" :key="token.address" :data="token"/>
     </div>
-
   </div>
-
 </template>
 
 <script>
@@ -44,6 +42,8 @@ export default {
       updateTimer: 0,
       error: null,
       received: false,
+      loaded: 30,
+      increment: 30,
       tokens: []
     }
   },
@@ -70,9 +70,26 @@ export default {
         await sleep(1000)
         this.updateTimer++
       }
+    },
+    getCurrentlyLoaded(array){
+      if(array.length >= this.loaded) return this.loaded
+      return array.length
     }
   },
+  async beforeMount() {
+    await this.requestTokens()
+  },
   async mounted() {
+    window.addEventListener("scroll", () => {
+      let bound = this.$refs.grid.children[this.$refs.grid.children.length-1].getBoundingClientRect()
+      if(bound.top <= innerHeight+100 && this.loaded < this.tokens.length){
+        this.loaded += this.increment
+      }
+      bound = this.$refs.grid.children[this.$refs.grid.children.length-this.increment].getBoundingClientRect()
+      if(bound.top > innerHeight+100 && this.loaded > this.increment) {
+        this.loaded -= this.increment
+      }
+    })
     await this.update()
   }
 }
